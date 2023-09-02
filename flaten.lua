809be3f7d945033a463c15be5060faafa09a8650
Darkz-001 <84206628+Darkz-001@users.noreply.github.com>
2023-09-02 19:18:57 +0000

    new file flaten.lua

---
+++ flaten.lua
@@ -0,0 +1,36 @@
+
+
+function main(l, w)
+    l, w = tonumber(l), tonumber(w)
+    for i = 1, w do
+        for j = 1, l do
+            mineForward()
+        end
+
+        local turn
+        if i%2 == 0 then
+            turn = turtle.turnLeft
+        else
+            turn = turtle.turnRight
+        end
+        
+        turn()
+        mineForward()
+        turn()
+    end
+end
+
+
+function mineForward()
+    while turtle.detect() do
+        turtle.dig()
+        sleep(0.1)
+    end
+    turtle.forward()
+    if turtle.detectUp() then
+        turtle.digUp()
+    end
+end
+
+
+main(...)
