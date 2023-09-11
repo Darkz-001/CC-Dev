a17bc987199a16a86104e0902fee8515422fa930
Darkz-001 <84206628+Darkz-001@users.noreply.github.com>
2023-09-11 00:44:42 +0000

    added a replace argument '-r'

--- install.lua
+++ install.lua
@@ -1,2 +1,7 @@
-program = ...
+program, extra = ...
+
+if extra == "-r" then
+    shell.run("delete", program .. ".lua")
+end
+
 shell.run("wget", "https://raw.githubusercontent.com/Darkz-001/CC-Dev/main/" .. program .. ".lua")
