545137ef88d2c0d93ab60d0ffda069c0881548e0
Darkz-001 <84206628+Darkz-001@users.noreply.github.com>
2025-01-26 21:30:42 +0000

    nil

--- eventListener.lua
+++ eventListener.lua
@@ -1,4 +1,13 @@
 while true do
     local event, side, channel, replyChannel, message, distance = os.pullEvent()
+    
+    -- overide nils with "nil" so they still print
+    if not event then event = "nil" end
+    if not side then side = "nil" end
+    if not channel then channel = "nil" end
+    if not replyChannel then replyChannel = "nil" end
+    if not message then message = "nil" end
+    if not distance then distance = "nil" end
+    
     print(event, side, channel, replyChannel, message, distance)
 end
