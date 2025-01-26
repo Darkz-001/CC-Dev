47803e7c020621da1e4ccda67a85e25c944ded1f
Darkz-001 <84206628+Darkz-001@users.noreply.github.com>
2025-01-26 03:04:25 +0000

    fixed? install

--- install.lua
+++ install.lua
@@ -1,6 +1,9 @@
-program, extra = ...
+local program, extra = ...
 
 if extra == "-r" or extra == "-u" then
+    while string.find(program, "/") do
+        program = string.sub(program, string.find(program, "/") + 1, -1)
+    end
     shell.run("delete", program .. ".lua")
 end
 
