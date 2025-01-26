26b7330b46b3afc4e3a16dbef70fae7777a2e786
Darkz-001 <84206628+Darkz-001@users.noreply.github.com>
2025-01-26 21:35:04 +0000

    make event listener better

--- eventListener.lua
+++ eventListener.lua
@@ -1,13 +1,8 @@
 while true do
-    local event, side, channel, replyChannel, message, distance = os.pullEvent()
+    local eventData = {os.pullEvent()}
     
-    -- overide nils with "nil" so they still print
-    if not event then event = "nil" end
-    if not side then side = "nil" end
-    if not channel then channel = "nil" end
-    if not replyChannel then replyChannel = "nil" end
-    if not message then message = "nil" end
-    if not distance then distance = "nil" end
-    
-    print(event, side, channel, replyChannel, message, distance)
+    for i, v in ipairs(eventData) do
+        print(i, v)
+    end
+    print("\n\n")
 end
